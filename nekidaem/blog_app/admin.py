6667d048e5b87c6ccb2993828f2a97ca4b9b82fd
Fiadel Majesty <aftonweb@gmail.com>
2019-01-12 08:27:19 +0600

    All project.

---
+++ nekidaem/blog_app/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Blog, BlogArticle
+
+
+class BlogAdminForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+    def clean(self):
+        if self.cleaned_data.get('owner') in self.cleaned_data.get('subscribers'):
+            raise ValidationError("You can't be subscribed to yourself.")
+        return super(BlogAdminForm, self).clean()
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
+    list_display = ('name', 'owner')
+
+
+@admin.register(BlogArticle)
+class BlogArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog', 'owner', 'created')
+    raw_id_fields = ('blog',)
+
+    def owner(self, obj):
+        return obj.blog.owner.username
